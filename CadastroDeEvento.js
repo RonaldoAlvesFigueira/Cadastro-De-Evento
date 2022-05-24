74c6891eb8d268150513434b94de2c5a13d63b37
Ronaldo Alves Figueira <ronaldoaf@bol.com.br>
2022-05-23 21:43:26 -0300

    first commit

---
+++ CadastroDeEvento.js
@@ -0,0 +1,17 @@
+let Idade = [22]
+if (Idade >= 18) {
+    console.log("Permitir Cadastro")
+}
+else {
+    console.log("Cadastro não permitido")
+}
+
+let participante = [80]
+
+if (participante <=100) {
+    console.log("Permitir evento")
+    
+}
+else {
+    console.log("Não foi possivel cadastrar devido excesso de participantes")
+}
